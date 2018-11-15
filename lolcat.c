8045837aa2c7955dbe0d10988a9fd8b0b1b1a390
Kevin Lau <kelau1993@gmail.com>
2018-11-15 14:49:57 -0800

    Move nested `helpstr_hack()` outside of `main()`
    Not sure why `helpstr_hack()` is a nested function. It causes compile
    errors. I can't find any sources that say nested functions are allowed
    in C. Moving it out still seems to work.

--- lolcat.c
+++ lolcat.c
@@ -75,6 +75,17 @@ void version()
     exit(0);
 }
 
+wint_t helpstr_hack(FILE * _ignored)
+{
+    (void)_ignored;
+    static size_t idx = 0;
+    char c = helpstr[idx++];
+    if (c)
+        return c;
+    idx = 0;
+    return WEOF;
+}
+
 int main(int argc, char** argv)
 {
     char* default_argv[] = { "-" };
@@ -131,17 +142,6 @@ int main(int argc, char** argv)
         wint_t (*this_file_read_wchar)(FILE*); /* Used for --help because fmemopen is universally broken when used with fgetwc */
         FILE* f;
         int escape_state = 0;
-
-        wint_t helpstr_hack(FILE * _ignored)
-        {
-            (void)_ignored;
-            static size_t idx = 0;
-            char c = helpstr[idx++];
-            if (c)
-                return c;
-            idx = 0;
-            return WEOF;
-        }
 
         if (!strcmp(*filename, "--help")) {
             this_file_read_wchar = &helpstr_hack;
